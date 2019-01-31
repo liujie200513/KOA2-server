39de3a4dcc922c77ce78bd42b0896f42b730b018
liujie200513 <531555297@qq.com>
2019-01-31 09:21:16 +0800

    添加忽略文件

--- middlewares/log.js
+++ middlewares/log.js
@@ -1,7 +1,6 @@
 function log( ctx ) {
     console.log( ctx.method, ctx.header.host + ctx.url )
 }
-
 module.exports = function () {
   return async function ( ctx, next ) {
     console.log("this is log:")
